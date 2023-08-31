2c5ca527e342cfe028f3a143a9f0943678a2e8d3
Jean <acalto2@gmail.com>
2023-08-31 20:21:03 +0200

    52. Sign in  func, controller object and resources

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../common/widgets/popup_messages.dart';
 import './notifier/sign_in_notifier.dart';
 
 class SignInController {
@@ -11,5 +13,35 @@ class SignInController {
     var state = ref.read(signInNotifierProvider);
     String email = state.email;
     String password = state.password;
+
+    if (state.email.isEmpty || email.isEmpty) {
+      toastInfo("Your email is empty");
+      return;
+    }
+    if ((state.password.isEmpty) || password.isEmpty) {
+      toastInfo("Your password is empty");
+      return;
+    }
+
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (credential.user == null) {
+        toastInfo('User not found');
+      }
+      if (!credential.user!.emailVerified) {
+        toastInfo('You must verify your email address first!');
+      }
+
+      var user = credential.user;
+      if (user != null) {
+        String? displayName = user.displayName;
+        String? email = user.email;
+        String? id = user.uid;
+        String? photoUrl = user.photoURL; 
+      }
+    } catch (e) {}
   }
 }
