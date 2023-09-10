ccab071e7369ba9bc0cf707d1d387019ec5aff8b
Jean <acalto2@gmail.com>
2023-09-10 23:37:29 +0200

    107,108. Build our post method

--- lib/common/services/http_util.dart
+++ lib/common/services/http_util.dart
@@ -2,31 +2,58 @@ import 'package:dio/dio.dart';
 import 'package:ulearning_app/global.dart';
 
 class HttpUtil {
-   late Dio dio;
+  late Dio dio;
 
   static final HttpUtil _instance = HttpUtil._internal();
 
   factory HttpUtil() {
     return _instance;
   }
-  HttpUtil._internal(){
-   BaseOptions options= BaseOptions(
-      baseUrl: "http://127.0.0.1:8000",
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 5),
-      headers: {},
-      contentType: "application/json: charset=utf-8",
-      responseType: ResponseType.json
-    );
-   dio = Dio(options);
+  HttpUtil._internal() {
+    BaseOptions options = BaseOptions(
+        baseUrl: "http://127.0.0.1:8000",
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+        headers: {},
+        contentType: "application/json: charset=utf-8",
+        responseType: ResponseType.json);
+    dio = Dio(options);
   }
 
-  Map<String, dynamic>? getAuthorizationHeader(){
+  Map<String, dynamic>? getAuthorizationHeader() {
     var headers = <String, dynamic>{};
     var accessToken = Global.storageService.getUserToken();
-    if(accessToken.isNotEmpty){
+    if (accessToken.isNotEmpty) {
       headers['Authorization'] = 'Bearer $accessToken';
     }
     return headers;
+  }
+
+  Future post(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    print("hit post method");
+
+    Options requestOptions = options ?? Options();
+    requestOptions.headers = requestOptions.headers ?? {};
+
+    Map<String, dynamic>? authorization = getAuthorizationHeader();
+
+    if (authorization != null) {
+      requestOptions.headers!.addAll(authorization);
+    }
+    print("done with header");
+   
+    var response = await dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: requestOptions,
+    );
+    print("done with post ${response.data["message"]}");
+    return response.data;
   }
 }
