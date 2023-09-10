2787214f3bf9a79fef6cdfc35ca114344a797e91
Jean <acalto2@gmail.com>
2023-09-10 19:36:34 +0200

    errs

--- lib/features/home/view/home.dart
+++ lib/features/home/view/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ulearning_app/common/widgets/image_widgets.dart';
 import 'package:ulearning_app/features/home/view/widgets/home_widget.dart';
 
 import '../../../common/widgets/search_widgets.dart';
